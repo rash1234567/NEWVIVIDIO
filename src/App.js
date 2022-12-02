386f3d57fdedafa70be2fd2861b661842838f3d9
rash1234567 <imamrasheedatahmad1993@gmail.com>
2022-12-02 13:56:07 -0800

    carouselCard done

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import Signup from "./components/Signup";
 import Login from "./components/Login";
 import React from "react";
 import SideNav from './components/SideNav';
+import TrendingMovie from './components/TrendingMovie';
+import CarouselCard from './components/CarouselCard';
 
 function App() {
   return (
     <>
-      <SideNav/>
     </>
   );
 }
