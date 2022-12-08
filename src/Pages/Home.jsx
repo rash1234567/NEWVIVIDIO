0a04ac842888febec1e4e1d544cb7a3394af9f36
Okunade sherif <o.d.alsherif@gmail.com>
2022-12-08 17:21:29 +0100

    update errors

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,13 +4,13 @@ import CarouselC from '../components/Carousel';
 import { useUserAuth } from '../utilities/UserAuthContextProvider';
 import YoutubePlayer from '../components/YoutubePlayer';
 
-
 function Home() {
    const {setTopRatedAPI,movieToprated,tvShowsToprated,url_movie,url_tv,setTrendingAPI,setType} = useUserAuth();
+   
 
   return (
 
-    <div className='lg:w-[60%] w-full h-[100vh] overflow-y-scroll overflow-x-hidden bg-[#171029] text-white home px-4 py-10'>
+    <div className='lg:w-[60%] w-full h-[100vh] overflow-y-scroll overflow-x-hidden bg-[#000] text-white home px-4 py-10'>
          <YoutubePlayer/>
         <div className="nav h-[7%] bg-[#000] w-full text-white font-bold ">
             <button onClick={()=> {setTopRatedAPI(tvShowsToprated);setTrendingAPI(url_tv);setType('tv')}}>Tv Shows</button>
