d6d3259b601c992aed73e3a73daddacdf6e1e20f
Okunade sherif <o.d.alsherif@gmail.com>
2022-12-09 13:24:54 +0100

    animation

--- src/Pages/Community.jsx
+++ src/Pages/Community.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { useNavigate } from "react-router-dom";
 
 function Community () {
-  const navigate = useNavigate();
   return (
-    <div className='lg:ml-[50vw] flex items-center justify-center'>
-      <h1 className='text-white text-5xl self-center comingSoon'>Coming soon....</h1>
+    <div className='lg:ml-[20vw] flex items-center justify-center w-full'>
+      <h1 className='text-white text-2xl md:text-5xl self-center comingSoon'>Coming soon....</h1>
     </div>
   )
 }
